b7d36491c0015f14a5ed97562e18ed6a234b459d
I304654 <r.slagers@student.fontys.nl>
2016-12-19 09:31:11 +0100

    Completed setting up Firebase integration

---
+++ Fontys-Dummys/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fontys-Dummys
+//
+//  Created by Ruud Slagers on 18/12/2016.
+//  Copyright © 2016 Ruud Slagers. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Fontys-Dummys/Pods-Fontys-Dummys-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fontys_Dummys : NSObject
+@end
+@implementation PodsDummy_Pods_Fontys_Dummys
+@end
