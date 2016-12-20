bd73993409159fe34fada6ed917285e1e452268f
I304654 <r.slagers@student.fontys.nl>
2016-12-20 14:23:20 +0100

    Sign in ui custom classes complete

---
+++ Fontys-Dummys/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Fontys-Dummys
+//
+//  Created by Ruud Slagers on 20/12/2016.
+//  Copyright © 2016 Ruud Slagers. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ Fontys-Dummys/FancyView.swift
@@ -0,0 +1,20 @@
+ //
+//  FancyView.swift
+//  Fontys-Dummys
+//
+//  Created by Ruud Slagers on 20/12/2016.
+//  Copyright © 2016 Ruud Slagers. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+         
+        
+    }
+
+}
