38254c8d5fb354f343254c288ea1a66d637ee89b
I304654 <r.slagers@student.fontys.nl>
2016-12-26 14:01:29 +0100

    Feed UI complete

---
+++ Fontys-Dummys/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Fontys-Dummys
+//
+//  Created by Ruud Slagers on 26/12/2016.
+//  Copyright © 2016 Ruud Slagers. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likeLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+
+
+
+}
